4735905a592683647f70644e1e915ca3ad880232
Philipp <me@p6.gg>
2025-08-12 17:29:29 +0200

    feat: always start a mockified word in lower-case

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,22 @@
 export const mockify = (input: string): string => {
-  const chars = input.split('')
-  const mockifiedChars = chars.map((c, i) => {
-    if (i % 2 === 0) {
-      return c.toLowerCase()
-    } else {
-      return c.toUpperCase()
-    }
-  })
+  const chars = input.split("");
 
-  return mockifiedChars.join('')
-}
+  let mockifiedChars = [] as string[];
+  let capitalized = false;
+  chars.forEach((c, i) => {
+    // if the character is a space, capitalization is set to true
+    if (c === " ") {
+      capitalized = true;
+      mockifiedChars.push(c);
+      return;
+    }
+
+    // whenever there is a letter from a-z or A-Z, the capitalization is toggled
+    if (c.match(/[a-zA-Z]/)) {
+      capitalized = !capitalized;
+    }
+    mockifiedChars.push(capitalized ? c.toUpperCase() : c.toLowerCase());
+  });
+
+  return mockifiedChars.join("");
+};
